6fc533723de8676c6e78909e68b46c50fa484b51
MuvvaSaiKarthik <54205485+MuvvaSaiKarthik@users.noreply.github.com>
2024-03-18 16:32:47 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,25 @@
+import streamlit as st
+st.set_page_config(layout='wide')
+st.title('DC PS SENTI DASHBOARD LOGIN')
+
+if "my_input" not in st.session_state:
+    st.session_state["my_input"] = ""
+
+username = st.text_input('Username')
+password = st.text_input('Password', type='password')
+
+my_input = username
+
+login = st.button('Login')
+
+if login:
+    if username == "VIKABH" and password == "VKB123":
+        st.success("Login successful!")
+        st.markdown('<span style="color: blue;">Click on home in side menu to see the data</span>', unsafe_allow_html=True)
+        st.session_state["my_input"] = my_input
+
+    else:
+        st.error("Invalid credentials. Please try again.")
+
+
+
